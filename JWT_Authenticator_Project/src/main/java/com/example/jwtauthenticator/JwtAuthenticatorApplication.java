3c4b0fc25933b212e1082e7398c450b6e6d9fee2
Yedukondalu <yedukondlu8978@gmail.com>
2025-07-11 11:25:54 +0530

    project has been successfully converted to generate a WAR file for deployment

--- JWT_Authenticator_Project/src/main/java/com/example/jwtauthenticator/JwtAuthenticatorApplication.java
+++ JWT_Authenticator_Project/src/main/java/com/example/jwtauthenticator/JwtAuthenticatorApplication.java
@@ -2,11 +2,18 @@ package com.example.jwtauthenticator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class JwtAuthenticatorApplication {
+public class JwtAuthenticatorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JwtAuthenticatorApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JwtAuthenticatorApplication.class, args);
