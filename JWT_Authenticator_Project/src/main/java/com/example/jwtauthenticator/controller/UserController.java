93c515670fba049814829302fc65ebfabf0f5f9f
psrikar045 <psrikar@stratapps.com>
2025-07-16 15:58:55 +0530

    minor code change

--- JWT_Authenticator_Project/src/main/java/com/example/jwtauthenticator/controller/UserController.java
+++ JWT_Authenticator_Project/src/main/java/com/example/jwtauthenticator/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.jwtauthenticator.controller;
 
+import com.example.jwtauthenticator.dto.BrandDataResponse;
 import com.example.jwtauthenticator.dto.UserIdDTO;
 import com.example.jwtauthenticator.dto.UserResponseDTO;
 import com.example.jwtauthenticator.service.CategoryService;
 import com.example.jwtauthenticator.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,9 +40,19 @@ public class UserController {
     private  UserService userService;
 
 
-    @PostMapping("/get-by-id") // A specific path for this operation
+    @PostMapping("/get-by-id") 
+    @Operation(
+            summary = "Get profile by ID",
+            description = "Retrieve complete user information",
+            security = { @SecurityRequirement(name = "Bearer Authentication") }
+        )
+        @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found", 
+                        content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+        })
     public ResponseEntity<UserResponseDTO> getUserByIdPost(@Valid @RequestBody UserIdDTO requestDTO) {
-        // Use the ID from the request body DTO
         return userService.getUserInfoByUserId(requestDTO.getId())
                 .map(userResponseDTO -> new ResponseEntity<>(userResponseDTO, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
